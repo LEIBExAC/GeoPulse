7baf6c2de67c1aa789f951643ca9cc9fd7cb8454
mohit-soni2003 <mohitsonip1847@gmail.com>
2025-07-08 16:40:37 +0530

    Keys updated to deploy

--- server/utility/generateTokenAndSetCookie.js
+++ server/utility/generateTokenAndSetCookie.js
@@ -13,9 +13,9 @@ const generateTokenAndSetCookie = (res, userId, user) => {
   );
   res.cookie("token", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expires in 7 days
-    httpOnly: true, // Ensure the cookie is only accessible via HTTP requests, not client-side JS
-    secure: true, // Set to false for development over HTTP
-    sameSite: "None", // Helps with cross-origin requests while keeping some protection
+    httpOnly: true, // Ensure the cookie is only accessible via HTTP requests, not client-side JS   prev false
+    secure: true, // Set to false for development over HTTP                                         pev false
+    sameSite: "None", // Helps with cross-origin requests while keeping some protection             lax
   });
 
   return token; // Return the token for further use
--- webclient/GeoPulse/src/assets/store/keyStore.js
+++ webclient/GeoPulse/src/assets/store/keyStore.js
@@ -1,3 +1,4 @@
-const backend_url = "http://localhost:8080";
+// const backend_url = "http://localhost:8080";
+const backend_url = "https://geopulse-165u.onrender.com/";
 
 export { backend_url };
